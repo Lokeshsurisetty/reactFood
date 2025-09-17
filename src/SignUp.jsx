071df8db0afbcb8b67f4f33bc3710d201354c3db
Lokeshsurisetty <surisettylokesh0123@gmail.com>
2025-09-17 12:36:36 +0530

    modified css in vsg

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import "./SignUp.css";
+import "./Signup.css";
 import { registerUser } from "./store";
 
 function SignUp() {
